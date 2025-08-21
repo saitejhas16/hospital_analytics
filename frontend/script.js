29bcf9124a6ea77d7fbe93c7d730423ac7e4c3a1
saitejhas49@gmail.com <saitejhas49@gmail.com>
2025-08-21 19:37:57 +0530

    Initial commit - hospital analytics

---
+++ frontend/script.js
@@ -0,0 +1,15 @@
+const BACKEND_URL = "https://hospital-analytics.onrender.com";  
+
+async function loadPatients() {
+  const res = await fetch(`${BACKEND_URL}/patients`);
+  const data = await res.json();
+  document.getElementById("patients").innerHTML =
+    "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+}
+
+async function loadDoctors() {
+  const res = await fetch(`${BACKEND_URL}/doctors`);
+  const data = await res.json();
+  document.getElementById("doctors").innerHTML =
+    "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+}
